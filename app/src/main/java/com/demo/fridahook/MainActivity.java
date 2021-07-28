09b8728a6006e9060d8d9cf534017f4a180328c6
wangbaohui <13802885224@139.com>
2021-07-28 15:37:44 +0800

    NetworkInterface方法hook可以反复调用

--- app/src/main/java/com/demo/fridahook/MainActivity.java
+++ app/src/main/java/com/demo/fridahook/MainActivity.java
@@ -13,6 +13,11 @@ import android.widget.TextView;
 import com.demo.HookCla1;
 import com.demo.HoolActiity;
 
+import java.lang.reflect.Method;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
 public class MainActivity extends AppCompatActivity {
     private EditText p1,p2;
     private Button b1,b2,b3;
@@ -51,7 +56,42 @@ public class MainActivity extends AppCompatActivity {
         b3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                NetworkInterface ni = null;
+                try {
+                    Enumeration<NetworkInterface> networks=NetworkInterface.getNetworkInterfaces ();
+                    String name ;
+                    while(networks != null && networks.hasMoreElements()){
+                        ni = networks.nextElement();
+                        if (!ni.isUp()){
+                            continue;
+                        }
+                        name = ni.getName();
+                        if (name != null && name.startsWith("wlan")){
+                            break;
+                        }
+                    }
+                    if (ni == null){
+                        ni= NetworkInterface.getByName("wlan0");
+                    }
+                    if(ni!=null){
+                        byte[] hw = (byte[]) callDeclaredMethod(ni, "getHardwareAddress", null, null);
+                        //					ni.getHardwareAddress();
+                        StringBuilder sb = new StringBuilder();
+                        if (hw != null) {
+                            int val = 0;
+                            for (int b : hw) {
+                                if (sb.length() > 0)
+                                    sb.append(':');
+                                val = b ;
+                                val &= 0x000000ff;
+                                sb.append(String.format("%02X", val));
+                            }
+                        }
+                    }
+                } catch (SocketException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
             }
         });
     }
@@ -73,4 +113,23 @@ public class MainActivity extends AppCompatActivity {
 //    private double add(double a, double b){
 //        return a+b;
 //    }
+public static Object callDeclaredMethod(Object obj, String methodname, Class<?> types[], Object values[]) {
+    if(obj==null){
+        return null;
+    }
+    Class<?> classz = obj.getClass();
+    Method method = null;
+    Object retValue = null;
+    try {
+        method = classz.getDeclaredMethod(methodname, types);
+        method.setAccessible(true);// 设置安全检查，设为true使得可以访问私有方法
+        retValue = method.invoke(obj, values);
+    } catch (NoSuchMethodException ex) {
+//	    AspLog.e(TAG, "callDeclaredMethod "+methodname+" reason=" +  ex +" "+ ex.getMessage());
+    } catch (Exception ex) {
+//        AspLog.e(TAG, "callDeclaredMethod "+methodname+" reason=" +  ex +" "+ ex.getMessage());
+    }
+    return retValue;
+}
+
 }
--- gadget_script.js
+++ gadget_script.js
@@ -4,14 +4,20 @@ console.log("Waiting for Java..");
 setImmediate(function() {
 Java.perform(function () {
    var Log = Java.use("android.util.Log");
-
-   var hook_class_HookCla2 = Java.use("com.demo.fridahook.HookCla2");
-   hook_class_HookCla2.showadd.implementation = function(){
-        Java.use("android.util.Log").v("Frida","获取showadd方法调用");//只能调用一次，调用第二次就崩溃,与这个代码无关
-        Java.use("android.util.Log").v("Frida",this+" kkk")//this是内存中的java对象
-        //目前问题，第一次调用直接崩溃，或者第二次调用就崩溃
-        return this.showadd();
+   Log.v("frida-lief", "Have fun!");
+   var hook_class_mac = Java.use("java.net.NetworkInterface");//可以反复调用
+   hook_class_mac.getHardwareAddress.implementation = function(){
+               Log.v("日志","获取 mac java.net.NetworkInterface.getHardwareAddress 方法调用");
+               Log.v("日志",Java.use("android.util.Log").getStackTraceString(Java.use("java.lang.Throwable").$new()));
+               return this.getHardwareAddress();
    }
+//   var hook_class_HookCla2 = Java.use("com.demo.fridahook.HookCla2");
+//   hook_class_HookCla2.showadd.implementation = function(){
+//        Java.use("android.util.Log").v("Frida","获取showadd方法调用");//只能调用一次，调用第二次就崩溃,与这个代码无关
+//        Java.use("android.util.Log").v("Frida",this+" kkk")//this是内存中的java对象
+//        //目前问题，第一次调用直接崩溃，或者第二次调用就崩溃
+//        return this.showadd();
+//   }
 //   hook_class_mac.add.implementation = function(a,b){
 //               Log.v("Frida","获取add方法调用 a="+a);
 //               return this.add(a,b);
